85149292d99b127d8c356bba513698964a448bb1
Chief5 <ben.helmy@gmail.com>
2021-03-16 17:09:48 +0100

    adding customer hook for dashbaord on customer

---
+++ nothing/public/js/customer.js
@@ -0,0 +1,10 @@
+try {
+  cur_frm.dashboard.add_transactions([
+    {
+        'label': 'Licences',
+        'items': [
+            'Licences'
+        ]
+    }
+]);
+} catch { /* do nothing for older versions */ }
